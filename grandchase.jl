61ac39c1db6b0a44c1815fd84e693b8c81623b3f
Ivan Nikitin <pierbezuhoff2016@gmail.com>
2019-11-23 20:29:43 +0300

    star -> enum

--- grandchase.jl
+++ grandchase.jl
@@ -1,36 +1,48 @@
-const Star = UInt8 # 1..6
-_format_star(star::Star)::String =
+import Base: *, -, show
+
+@enum Star::UInt8 ☆=0x01 ☆☆ ☆☆☆ ☆☆☆☆ ☆☆☆☆☆ ☆☆☆☆☆☆
+*(n::Int, s::Star)::Star =
+    Star(n * UInt8(s))
+-(s0::Star, s::Star)::Int8 =
+    Int8(s0) - Int8(s)
+show(io::IO, star::Star) =
     "$(Int(star))☆"
 
 struct MonsterCard
     star::Star
 end
-Base.show(io::IO, card::MonsterCard) =
-    print(io, "MC $(_format_star(card.star))")
+MC(i::Int)::MonsterCard =
+    MonsterCard(Star(i))
+show(io::IO, card::MonsterCard) =
+    print(io, "MC $(card.star)")
 
 const UpgradeLevel = UInt8 # 0..12
+struct Lvl end
+const L = Lvl()
+*(n::Int, l::Lvl)::UpgradeLevel =
+    UpgradeLevel(n)
 
 struct Hero
     star::Star
     upgrade::UpgradeLevel
 end
-Base.show(io::IO, hero::Hero) =
-    print(io, "H $(_format_star(hero.star)) $(Int(hero.upgrade))L")
+show(io::IO, hero::Hero) =
+    print(io, "H $(hero.star) $(Int(hero.upgrade))L")
 
-@enum HeroRank::UInt8 A=0x01 S=0x02 SR=0x03
+@enum HeroRank::UInt8 A S SR
 
 struct RankedHero
     rank::HeroRank
     star::Star
     upgrade::UpgradeLevel
 end
-Base.show(io::IO, hero::RankedHero) =
-    print(io, "H $(hero.rank) $(_format_star(hero.star)) $(Int(hero.upgrade))L")
+show(io::IO, hero::RankedHero) =
+    print(io, "H $(hero.rank) $(hero.star) $(Int(hero.upgrade))L")
 
 const Prob = Float16
 const ProbBonus = Prob
 
-function _rel_prob(d::UInt8)::Prob
+function _rel_prob(d::Int8)::Prob
     if d < 0x01 Prob(1)
     elseif d == 0x01 Prob(1/2)
     elseif d == 0x02 Prob(1/4)
@@ -44,7 +56,7 @@ function rel_prob(star::Star, card::MonsterCard)::Prob
     _rel_prob(star - card.star)
 end
 
-function _rel_prob_bonus(d::UInt8)::ProbBonus
+function _rel_prob_bonus(d::Int8)::ProbBonus
     if d < 0x01 ProbBonus(0)
     elseif d == 0x01 ProbBonus(15/100)
     elseif d == 0x02 ProbBonus(7/100)
@@ -56,4 +68,13 @@ end
 
 function rel_prob_bonus(star::Star, card::MonsterCard)::ProbBonus
     _rel_prob_bonus(star - card.star)
+end
+
+function star_cap(star::Star)::UpgradeLevel
+    if star == ☆☆☆ UpgradeLevel(3)
+    elseif star == ☆☆☆☆ UpgradeLevel(6)
+    elseif star == ☆☆☆☆☆ UpgradeLevel(9)
+    elseif star == ☆☆☆☆☆☆ UpgradeLevel(12)
+    else UpgradeLevel(0) # bad
+    end
 end
