e125ad7ef56cafc5953f781bb31929ad98cabb0a
Ivan Nikitin <pierbezuhoff2016@gmail.com>
2019-11-24 03:54:51 +0300

    l

--- grandchase.jl
+++ grandchase.jl
@@ -19,11 +19,11 @@ show(io::IO, card::MonsterCard) =
 const UpgradeLevel = UInt8 # 0..12
 struct LvlSymbol end
 const L = LvlSymbol()
-*(n::Int, l::LvlSymbol)::UpgradeLevel =
+*(n::Int, ::LvlSymbol)::UpgradeLevel =
     UpgradeLevel(n)
 
 struct Hero
-    star::Star
+    star::Star # 3..6
     upgrade::UpgradeLevel
 end
 show(io::IO, hero::Hero) =
@@ -39,15 +39,16 @@ end
 show(io::IO, hero::RankedHero) =
     print(io, "H $(hero.rank) $(hero.star) $(Int(hero.upgrade))L")
 
+# const Prob = Rational{Int} # try/cmp performance
 const Prob = Float16
 const ProbBonus = Prob
 
 function _rel_prob(d::Int8)::Prob
     if d < 0x01 Prob(1)
-    elseif d == 0x01 Prob(1/2)
-    elseif d == 0x02 Prob(1/4)
-    elseif d == 0x03 Prob(1/10)
-    elseif d == 0x04 Prob(1/100)
+    elseif d == 0x01 Prob(1//2)
+    elseif d == 0x02 Prob(1//4)
+    elseif d == 0x03 Prob(1//10)
+    elseif d == 0x04 Prob(1//100)
     else Prob(0)
     end
 end
@@ -58,10 +59,10 @@ end
 
 function _rel_prob_bonus(d::Int8)::ProbBonus
     if d < 0x01 ProbBonus(0)
-    elseif d == 0x01 ProbBonus(15/100)
-    elseif d == 0x02 ProbBonus(7/100)
-    elseif d == 0x03 ProbBonus(3/100)
-    elseif d == 0x04 ProbBonus(1/1000)
+    elseif d == 0x01 ProbBonus(15//100)
+    elseif d == 0x02 ProbBonus(7//100)
+    elseif d == 0x03 ProbBonus(3//100)
+    elseif d == 0x04 ProbBonus(1//1000)
     else ProbBonus(0)
     end
 end
@@ -82,12 +83,8 @@ end
 struct Probably{T}
     prob::Prob
     value::T
-const Distrib{T} = Dict{T, Prob}
-
-struct HeroUpgradeState
-    prob_bonus::ProbBonus
-    hero::Hero
 end
+const Distrib{T} = Dict{T, Prob}
 
 struct HeroUpgradeResult
     prob_bonus::ProbBonus
@@ -95,9 +92,17 @@ struct HeroUpgradeResult
     rest::Vector{MonsterCard}
 end
 
-function simulate_hero_upgrade(lvl::UpgradeLevel, target::UpgradeLevel, cards::Vector{MonsterCard})::HeroUpgradeResult
-    while lvl < target && !isempty(cards)
-        1
+function simulate_hero_upgrade(hero::Hero, target::Hero, cards::Vector{MonsterCard})::HeroUpgradeResult
+    @assert target.upgrade > hero.upgrade "target hero level < current level"
+    @assert hero.upgrade <= star_cap(hero.star) && target.upgrade <= star_cap(target.star) "level > start cap"
+    prob_bonus = ProbBonus(0)
+    lvl = hero.upgrade
+    star = hero.star
+    target_lvl = target.upgrade
+    if isempty(cards) || lvl >= target_lvl
+        return HeroUpgradeResult(prob_bonus lvl, cards)
     end
-    undef
+    #for card in cards
+    #end
+    return HeroUpgradeResult(prob_bonus, lvl, Vector{MonsterCard}())
 end
